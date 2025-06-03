2a231801961abc56fb705808a228367d96153129
Dibbiii <alessandra.dibella@studio.unibo.it>
2025-06-03 14:41:50 +0200

    Add UserApiCall model for user API interactions

---
+++ src/main/java/alessandra_alessandro/ketchapp_bff/models/apicall/UserApiCall.java
@@ -0,0 +1,29 @@
+package alessandra_alessandro.ketchapp_bff.models.apicall;
+
+import alessandra_alessandro.ketchapp_bff.models.responses.UserResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserApiCall {
+    
+    private UUID uuid;
+    
+    private String username;
+    
+    private String email;
+    
+    private String firebaseUid;
+    
+    private Timestamp createdAt;
+
+}
