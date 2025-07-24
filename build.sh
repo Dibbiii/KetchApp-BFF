ebef4744e9667452292c5ec660523b189611d4b1
Alessandro Bruno <67830217+alessandrobrunoh@users.noreply.github.com>
2025-07-24 15:07:38 +0200

    Add Windows build script and update Docker Compose usage

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ echo "Building Docker image from Dockerfile..."
 docker build -t ketchapp-bff-api .
 
 echo "Stopping and removing existing Docker containers..."
-docker-compose down --rmi all -v --remove-orphans
+docker compose down --rmi all -v --remove-orphans
 
 echo "Starting Docker Compose..."
-docker-compose up --build
-
+docker compose up --build
